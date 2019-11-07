3c178391c75cd95aea1e461aa8780986ecd224c2
Keelan <Keelan.OConnor@students.ittralee.ie>
2019-11-07 10:17:50 +0000

    GUI changes
    Changed GUI slightly, will improve it later.

--- Keelan_OConnor_OOP_Project/src/Synth.java
+++ Keelan_OConnor_OOP_Project/src/Synth.java
@@ -1,6 +1,7 @@
 import javax.sound.midi.*;
 import javax.sound.midi.Synthesizer;
 import javax.swing.*;
+import java.awt.*;
 
 //TODO Implement proper inheritance
 //TODO Write base features of driver
@@ -13,15 +14,26 @@ import javax.swing.*;
 public class Synth {
     public static void main(String[] args) {
 
-        //TODO make nicer GUI
+        //TODO Screen size for GUI.width and height
+
+        //TODO make nicer GUI with flow layout
         //GUI Setup
-        JFrame frame1 = new JFrame("Synth1");
-        frame1.setBounds(400,400,400,400); //Location + position of GUI
+        JFrame frame1 = new JFrame("Synthesizer");
+        frame1.setBounds(400,250,400,400); //Location + position of GUI
+
         JPanel panel1 = new JPanel();
+        JLabel label1 = new JLabel("Label");
+        JTextArea textArea1 = new JTextArea();
+        textArea1.setBounds(1,1,200,200);//TODO Make this work
+        textArea1.setText("This is a text area\nssssss\n\n\n\n\n\naaaaa"); //TODO Make this work
+        textArea1.setEditable(false);
+
         JButton button1 = new JButton("Play");
         panel1.add(button1);
         frame1.add(panel1);
         frame1.setDefaultCloseOperation(frame1.EXIT_ON_CLOSE);
+        frame1.setLayout(new FlowLayout());//TODO Make this work
+
         frame1.setVisible(true);
 
         //Declaring a sequencer and synthesizer
