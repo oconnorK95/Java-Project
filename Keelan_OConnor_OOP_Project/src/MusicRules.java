0c4b01482bb7f45d7b075a97c85bb9d434ccefa0
Keelan <Keelan.OConnor@students.ittralee.ie>
2019-11-07 09:59:10 +0000

    Create MusicRules.java
    Created a file to help me remember what rules the music must conform to. I can flesh it out later to generate acceptable music

---
+++ Keelan_OConnor_OOP_Project/src/MusicRules.java
@@ -0,0 +1,12 @@
+public class MusicRules {
+    //Need to keep track of what rules my program will work around, might delete if unnecessary.
+
+    //Tempo: Speed of the song
+    //Key: Determines the "Range of Notes" that can be played
+    //Chords: Determined by key, made of 3 notes
+    //Triads: A group of chords, must calculate which triads are acceptable.  I ii iii IV V vi vii MAJOR minor
+    //Length: The duration of the music
+
+
+
+}
