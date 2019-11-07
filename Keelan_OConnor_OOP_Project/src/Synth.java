59d99d6170b3d4cacb2d8b01ed6b3b0abe695099
Keelan <Keelan.OConnor@students.ittralee.ie>
2019-11-07 09:47:12 +0000

    Planning + Cleaning up
    Created TODO comments, tidied up, fixed depreciate GUI method.

--- Keelan_OConnor_OOP_Project/src/Synth.java
+++ Keelan_OConnor_OOP_Project/src/Synth.java
@@ -1,15 +1,30 @@
 import javax.sound.midi.*;
 import javax.sound.midi.Synthesizer;
+import javax.swing.*;
 
-//Need proper inheritance + driver
-//What are the base features of a synthesizer, what variations will it need, what will the driver do
-//What class will generate music?
-//What class will read/write music files
+//TODO Implement proper inheritance
+//TODO Write base features of driver
+//TODO Music generation function
+//TODO Define rules music must follow
+//TODO Read/Write music function
+//TODO Learn how to use Channels
+
 
 public class Synth {
     public static void main(String[] args) {
 
+        //TODO make nicer GUI
+        //GUI Setup
+        JFrame frame1 = new JFrame("Synth1");
+        frame1.setBounds(400,400,400,400); //Location + position of GUI
+        JPanel panel1 = new JPanel();
+        JButton button1 = new JButton("Play");
+        panel1.add(button1);
+        frame1.add(panel1);
+        frame1.setDefaultCloseOperation(frame1.EXIT_ON_CLOSE);
+        frame1.setVisible(true);
 
+        //Declaring a sequencer and synthesizer
         Sequencer sequencer1;
         Synthesizer synthesizer1;
 
@@ -19,24 +34,13 @@ public class Synth {
             synthesizer1 = MidiSystem.getSynthesizer();
             synthesizer1.open(); //TODO make this always open
             System.out.println("Tried successfully");
-        }
+        }//End try
 
 
         //Catch exceptions for unavailable sequencer and synthesizer
         catch(MidiUnavailableException e){
-        }
+        }//End catch
 
-    }
-/*
-    JFrame frame1 = new JFrame("Synth1");
-    JFrame.setSize(100,100);
-    JPanel panel1 = new JPanel();
-    JButton button1 = new JButton("Play");
-
-    panel1.add(button1);
-    frame1.add(panel1);
-    frame1.setDefaultCloseOperation(frame1.EXIT_ON_CLOSE);
-    frame1.show();
-*/
+    }//End main
 
 }//End Synth
